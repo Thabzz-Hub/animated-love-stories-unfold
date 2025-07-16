521045e746198aea8f1049d4c24b425ba0f7e5c0
Thabzz-Hub <st10446518@rcconnect.edu.za>
2025-07-16 20:23:49 +0200

    Updated the page with baby U changes 🔥

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,19 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/animated-love-stories-unfold/' : '/', // 👈 THIS IS THE IMPORTANT LINE
+  base: mode === 'production' ? '/animated-love-stories-unfold/' : '/', 
 
   server: {
     host: "::",
     port: 8080,
   },
-  
   plugins: [
     react(),
     mode === 'development' && componentTagger()
   ].filter(Boolean),
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+}))
